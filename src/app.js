2d8b3ca651e4ab01ca54c8824750e1dc4a3a35fa
Bia Luz <anabluz08@gmail.com>
2023-04-18 19:32:52 -0300

    Requisito: post de usuario

--- src/app.js
+++ src/app.js
@@ -1,11 +1,71 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ObjectId } from "mongodb";
+import dayjs from "dayjs"
+//import dotenv from "dotenv";
+import joi from "joi";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+//dotenv.config();
 
 
+// Conexão com o banco de dados 
 
+const mongoClient = new MongoClient("mongodb://localhost:27017/batePapoUol")
+try {
+    await mongoClient.connect()
+    console.log('MongoDB conectado!')
+} catch (err) {
+    console.log(err.message)
+}
+
+const db = mongoClient.db()
+
+
+//Validações
+const participantSchema = joi.object({ name: joi.string().required() })
+
+const messageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid("message", "private_message")
+}) 
+ 
+// endpoints
+
+app.post("/participants", async (req, res) => {
+  const name = req.body.name;
+  const validation = participantSchema.validate(req.body)
+  
+  if (validation.error) {
+      return res.sendStatus(422)
+  }
+
+  try {
+      const nomeEmUso  = await db.collection("participants").findOne({ name })
+      if (nomeEmUso) return res.sendStatus(409)
+
+      const lastStatus = Date.now()
+      await db.collection("participants").insertOne({ name, lastStatus: lastStatus })
+
+      const message = {    
+          from: name,
+          to: 'Todos',
+          text: 'entra na sala...',
+          type: 'status',
+          time: dayjs(lastStatus).format("HH:mm:ss")
+      }
+
+      await db.collection("messages").insertOne(message)
+      res.sendStatus(201)
+
+  } catch (err) {
+      res.status(500).send(err.message)
+  }
+})
 
 
 
