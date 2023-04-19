2c5e876c290d236df450a322827d8c21ef0ca02c
Bia Luz <anabluz08@gmail.com>
2023-04-19 02:42:40 -0300

    Requisistos:remoção de usuários inativos

--- src/app.js
+++ src/app.js
@@ -115,9 +115,83 @@ app.post("/messages", async (req, res) => {
 
   } catch (err) {
       console.log(err);
-      return res.status(500).send('Internal server error');
+      return res.status(500).send('Erro do servidor');
   }
 })
+
+
+app.get('/messages', async (req, res) => {
+  
+  const menssagens = db.collection('messages');
+  const limite = req.query.limit;
+
+  if (limite && (isNaN(parseInt(limite)) || parseInt(limite) <= 0)) {
+      return res.status(422).send({ error: 'Limite precisa ser positivo' });
+  }
+
+  const ultimasMenssagens = await menssagens
+      .find({
+          $or: [
+              { to: 'Todos' },
+              { from: req.headers.user },
+              { to: req.headers.user },
+          ],
+      })
+      .sort({ time: -1 })
+      .limit(limite ? parseInt(limite) : 0)
+      .toArray();
+
+  res.send(ultimasMenssagens.reverse());
+});
+
+app.post("/status", async (req, res) => {
+
+  const participants = db.collection('participants');
+  const user = req.header("User");
+  
+  if (!user) {
+      return res.status(404).send();
+  }
+
+  const participant = await participants.findOne({ name: user });
+  if (!participant) {
+      return res.status(404).send();
+  }
+
+  try {
+      await participants.updateOne(
+          { name: user },
+          { $set: { lastStatus: Date.now() } }
+      );
+      return res.send();
+  } catch (error) {
+      console.error(error);
+      return res.status(500).send();
+  }
+});
+
+setInterval(async () => {
+  const participantes= db.collection('participants');
+  const menssagens = db.collection('messages');
+
+  const time = dayjs().subtract(10, 'seconds').valueOf();
+
+  const participanteInativo = await participantes.find({ lastStatus: { $lt: time } }).toArray();
+
+  participanteInativo.forEach(async (participant) => {
+      const menssagem = {
+          from: participant.name,
+          to: 'Todos',
+          text: 'sai da sala...',
+          type: 'status',
+          time: dayjs().format('HH:mm:ss')
+      };
+
+      await menssagens.insertOne(menssagem);
+      await participantes.deleteOne({ _id: participant._id });
+  });
+
+}, 15000);
 
 
 
