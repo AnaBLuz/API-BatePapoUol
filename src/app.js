69662865785dad91ac0d63d7b2e85ca2f6bea700
Bia Luz <anabluz08@gmail.com>
2023-04-19 01:39:07 -0300

    Requisito: postar mensagem

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const db = mongoClient.db()
 const participantSchema = joi.object({ name: joi.string().required() })
 
 const messageSchema = joi.object({
-    from: joi.string().required(),
+    from: joi.string(),
     to: joi.string().required(),
     text: joi.string().required(),
     type: joi.string().required().valid("message", "private_message")
@@ -75,7 +75,48 @@ app.get("/participants", async (req, res) => {
   catch(err){
     res.status(500).send(err.message)
   }
+})
 
+app.post("/messages", async (req, res) => {
+
+  const messagens = db.collection('messages');
+  const participantes = db.collection('participants');
+
+  try {
+      const { to, text, type } = req.body;
+      const { user } = req.headers
+      
+
+      const { error } = messageSchema.validate({ to, text, type });
+
+      if (error) {
+          return res.status(422).send(error.details[0].message);
+      }
+
+      const participanteExiste = await participantes.findOne({
+          name: user
+      });
+
+      if (!participanteExiste) {
+          return res.status(422).send("Remetente não encontrado");
+      }
+
+      const time = dayjs().format("HH:mm:ss");
+      const message = {
+          from: user,
+          to,
+          text,
+          type,
+          time,
+      }
+
+      await messagens.insertOne(message)
+      return res.status(201).send();
+
+  } catch (err) {
+      console.log(err);
+      return res.status(500).send('Internal server error');
+  }
 })
 
 
